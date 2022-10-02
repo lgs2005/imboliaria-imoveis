0225ccc529e32145efb55c9d66b1cd7473d1bbd4
lgs2005 <lgs22264@gmail.com>
2022-10-02 13:15:50 -0300

    top 200 maneiras de Perder Tempo

--- init.py
+++ init.py
@@ -27,12 +27,6 @@ NOME_PROJETO = 'imboliahria'
 app = Flask(NOME_PROJETO)
 bcrypt = Bcrypt(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{NOME_PROJETO}.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-db.session.execute('PRAGMA FOREIGN_KEYS=ON')
-
 app.config['JWT_SECRET_KEY'] = 'b25aeed15e1a4f3e9837125d8a815b86-casanapraia-2c040adb0beb40cebf4af3276028acee'
 app.config['JWT_TOKEN_LOCATION'] = ['headers']
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1, minutes=30)
--- modelos.py
+++ modelos.py
@@ -1,38 +1,67 @@
-from init import db
+from datetime import datetime
+from typing import TYPE_CHECKING
+from init import app, NOME_PROJETO
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.orm import Mapped
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{NOME_PROJETO}.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+if TYPE_CHECKING:
+    import flask_sqlalchemy
+    import sqlalchemy
+    import sqlalchemy.orm
+
+    from typing_extensions import Self
+
+    class Model(flask_sqlalchemy.Model):
+        query: flask_sqlalchemy.BaseQuery[Self]
+
+    class Database(flask_sqlalchemy.SQLAlchemy):
+        Model: type[Model]
+        relationship: type[sqlalchemy.orm.relationship]
+
+    db: Database
+
+db = SQLAlchemy(app)
+db.session.execute('PRAGMA FOREIGN_KEYS=ON')
 
 
 class Cliente(db.Model):
-    '''Representa um cliente'''
-    id = Column(Integer, primary_key=True)
+    '''Representa um cliente''' 
+    id:             Mapped[int]             = Column(Integer, primary_key=True)
 
-    nome = Column(String(255), nullable=False)
-    email = Column(String(255), nullable=False, unique=True)
-    cpf = Column(String(11), nullable=False, unique=True)
-    telefone = Column(String(20), nullable=False, unique=True)
-    hash_senha = Column(String(60), nullable=False)
+    nome:           Mapped[str]             = Column(String(255), nullable=False)
+    email:          Mapped[str]             = Column(String(255), nullable=False, unique=True)
+    cpf:            Mapped[str]             = Column(String(11), nullable=False, unique=True)
+    telefone:       Mapped[str]             = Column(String(20), nullable=False, unique=True)
+    hash_senha:     Mapped[str]             = Column(String(60), nullable=False)
 
 
 class Imovel(db.Model):
     '''Representa os dados de um imóvel'''
-    id = Column(Integer, primary_key=True)
 
-    nome = Column(String(255), nullable=False)
-    descricao = Column(String(1000), nullable=False)
-    local = Column(String(255), nullable=False)
-    area = Column(Integer, nullable=False)
+    id:             Mapped[int]             = Column(Integer, primary_key=True)
 
-    venda = db.relationship('Venda', back_populates='imovel', uselist=False)
+    nome:           Mapped[str]             = Column(String(255), nullable=False)
+    descricao:      Mapped[str]             = Column(String(1000), nullable=False)
+    local:          Mapped[str]             = Column(String(255), nullable=False)
+    area:           Mapped[int]             = Column(Integer, nullable=False)
+
+    venda:          Mapped['Venda']         = db.relationship('Venda', back_populates='imovel', uselist=False)
 
 
 class Venda(db.Model):
     '''Representa a venda de um imóvel'''
-    id = Column(ForeignKey(Imovel.id), primary_key=True)
 
-    imovel = db.relationship(Imovel, back_populates='venda', uselist=False)
-    preco = Column(Integer, nullable=False)
+    id:             Mapped[int]             = Column(ForeignKey(Imovel.id), primary_key=True)
+    imovel:         Mapped[Imovel]          = db.relationship(Imovel, back_populates='venda', uselist=False)
 
-    tipo = Column(String(20))
+    preco:          Mapped[int]             = Column(Integer, nullable=False)
+    tipo:           Mapped[str]             = Column(String(20))
+    
     __mapper_args__ = {
         'polymorphic_on': tipo,
         'polymorphic_identity': 'venda'
@@ -41,61 +70,67 @@ class Venda(db.Model):
 
 class VendaRealizada(Venda):
     '''Uma venda que foi realizada'''
+
     __mapper_args__ = {
         'polymorphic_identity': 'realizada'
     }
 
-    data = Column(DateTime, nullable=False)
+    cliente_id:     Mapped[int]             = Column(ForeignKey(Cliente.id))
+    cliente:        Mapped[Cliente]         = db.relationship(Cliente)
 
-    cliente_id = Column(ForeignKey(Cliente.id))
-    cliente = db.relationship(Cliente)
+    data:           Mapped[datetime]        = Column(DateTime, nullable=False)
 
 
 class VendaAlugel(Venda):
     '''Venda através de alugeis'''
+
     __mapper_args__ = {
         'polymorphic_identity': 'alugel'
     }
 
-    alugado = Column(Boolean, nullable=False)
-    alugeis = db.relationship('alugeis', back_populates='venda')
+    alugado:        Mapped[bool]            = Column(Boolean, nullable=False)
+    alugeis:        Mapped[list['Alugel']]  = db.relationship('alugeis', back_populates='venda')
 
 
 class Alugel(db.Model):
     '''Um alugel realizado'''
-    id = Column(Integer, primary_key=True)
 
-    venda_id = Column(ForeignKey(VendaAlugel.id))
-    venda = db.relationship(VendaAlugel, back_populates='alugeis')
+    id:             Mapped[int]             = Column(Integer, primary_key=True)
 
-    cliente_id = Column(ForeignKey(Cliente.id))
-    cliente = db.relationship(Cliente)
+    venda_id:       Mapped[int]             = Column(ForeignKey(VendaAlugel.id))
+    venda:          Mapped[VendaAlugel]     = db.relationship(VendaAlugel, back_populates='alugeis')
 
-    data = Column(DateTime, nullable=False)
-    data_fim = Column(DateTime, nullable=False)
+    cliente_id:     Mapped[int]             = Column(ForeignKey(Cliente.id))
+    cliente:        Mapped[Cliente]         = db.relationship(Cliente)
+
+    data:           Mapped[datetime]        = Column(DateTime, nullable=False)
+    data_fim:       Mapped[datetime]        = Column(DateTime, nullable=False)
 
 
 class VendaLeilao(Venda):
     '''Venda através de leilao'''
+
     __mapper_args__ = {
         'polymorphic_identity': 'leilao'
     }
 
-    data_fim = Column(DateTime, nullable=False)
-    apostas = db.relationship('Aposta', back_populates='venda')
+    data_fim:       Mapped[datetime]        = Column(DateTime, nullable=False)
+    apostas:        Mapped[list['Aposta']]  = db.relationship('Aposta', back_populates='venda')
 
-    vencedor_id = Column(ForeignKey('aposta.id'), nullable=True)
-    vencedor = db.relationship('Aposta')
+    vencedor_id:    Mapped[int]             = Column(ForeignKey('aposta.id'), nullable=True)
+    vencedor:       Mapped['Aposta']        = db.relationship('Aposta')
 
 
 class Aposta(db.Model):
     '''Aposta feita em um leilao'''
-    id = Column(Integer, primary_key=True)
 
-    cliente_id = Column(ForeignKey(Cliente.id))
-    cliente = db.relationship(Cliente)
+    id:             Mapped[int]             = Column(Integer, primary_key=True)
 
-    leilao_id = Column(ForeignKey(VendaLeilao.id))
-    leilao = db.relationship(VendaLeilao, back_populates='apostas')
+    cliente_id:     Mapped[int]             = Column(ForeignKey(Cliente.id))
+    cliente:        Mapped[Cliente]         = db.relationship(Cliente)
 
-    valor = Column(Integer, nullable=False)
+    leilao_id:      Mapped[int]             = Column(ForeignKey(VendaLeilao.id))
+    leilao:         Mapped[VendaLeilao]     = db.relationship(VendaLeilao, back_populates='apostas')
+
+    valor:          Mapped[int]             = Column(Integer, nullable=False)
+    data:           Mapped[datetime]        = Column(DateTime, nullable=False)
